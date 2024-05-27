69f6d3acab2f67580c0b83cad63117971acbf9f9
Yuliia Pavliv <juli.pavliv@gmail.com>
2024-05-27 19:27:58 +0200

    Create project build

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,13 +1,14 @@
 import { ApplicationConfig, isDevMode } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
-import { routes } from './app.routes';
 import { provideServiceWorker } from '@angular/service-worker';
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
+import { LocationStrategy, HashLocationStrategy } from '@angular/common';
+import { routes } from './app.routes';
 import { MockInterceptor } from './interceptors/mock.interceptor';
 
 export const appConfig: ApplicationConfig = {
   providers: [
+    { provide: LocationStrategy, useClass: HashLocationStrategy },
     provideRouter(routes),
     provideServiceWorker('ngsw-worker.js', {
       enabled: !isDevMode(),
