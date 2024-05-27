36c27e8cfa1b507d0760d2915733bfbb18e136f6
JjuliaP <juli.pavliv@gmail.com>
2024-05-27 16:38:26 +0300

    Add page not found component (#7)
    Co-authored-by: Julia Julia <juliapc@Julias-MBP.home>

--- src/app/modules/user-edit/user-edit.module.ts
+++ src/app/modules/user-edit/user-edit.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { UserEditComponent } from './components/user-edit/user-edit.component';
+import { UserEditRoutingModule } from './user-edit-routing.module';
 
 @NgModule({
   declarations: [UserEditComponent],
-  imports: [CommonModule],
+  imports: [CommonModule, UserEditRoutingModule],
 })
 export class UserEditModule {}
